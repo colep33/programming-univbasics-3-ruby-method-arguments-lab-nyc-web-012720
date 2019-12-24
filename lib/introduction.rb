d25bb47bd5c67ecf15bb024fe9f3973c837311ca
Cole Arthur Parham <coleparham@gmail.com>
2019-12-24 19:12:04 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,9 +10,8 @@ def introduction_with_language(name,language)
   puts "Hi, my name is #{name}, and I am learning to program in #{language}."
 end
 
-
+language = "Ruby"
 def introduction_with_language_optional(name,language)
-  language = "Ruby"
   puts "hi, my name is #{name}, and I am learning to program in #{language}"
 end
 
